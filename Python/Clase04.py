30e2019f4cf00c9c62dc88f38cc1077d9310d39c
Sebastian-Betancourt <168014602+Sebastian-Betancourt@users.noreply.github.com>
2024-05-15 12:04:20 -0500

    Add files via upload

---
+++ Python/Clase04.py
@@ -0,0 +1,21 @@
+print("Instagram")
+user = input("Phone number, user name, or email")
+password = input("Password")
+
+if user == "Sebas" and password == "06":
+    print(f"Welcom{user}")
+    print("Loading your posts")
+else:
+    print("Sory, your password was incorrect, please double-check your password.")
+
+print("Sign up to see photos and videos from your friends")
+input("Login with facebook")
+facebook = input("yes or no")
+if facebook == "yes":
+    print("Welcom")
+
+else:
+    mobile=input("Mobil number or email")
+    name=input("Full name")
+    username=input("username")
+    password=input("password")
